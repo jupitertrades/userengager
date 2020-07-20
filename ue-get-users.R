320b9dbfa1e8e5e8060d27b4d5d0e242b7e15955
simit patel <simitp@gmail.com>
2020-07-20 12:45:54 -0400

    update

---
+++ ue-get-users.R
@@ -0,0 +1,30 @@
+#' @title ethereum token content generator
+#'
+#' @description writes research reports for erc20 tokens
+#'
+#' @param address
+#'
+#' @return NULL
+#'
+#' @examples ue_get_users()
+#'
+#' @export ue_get_users
+
+ue_get_users_lists <- function(ue_id,ue_key = Sys.getenv('ue_key')) {
+  all_users_list <- GET(glue('https://{ue_id}.user.com/api/public/users/'),
+                             add_headers(`Authorization` = glue('Token {ue_key}'))) %>% 
+                          content() %>% pluck('results') 
+  for(i in 1:length(all_users_list)) {
+    adf <- all_users_list[[i]] %>% compact() %>% bind_rows()
+    if(!is.null(adf$lists)) {
+      adf2_list <- adf$lists %>% bind_rows() %>% pull(id) %>% glue_collapse(sep =',',last=',')
+      adf$lists = adf2_list
+    }
+    if(is.null(adf$lists)) {
+      adf$lists <-  '0'
+    }
+    all_users_df <- bind_rows(all_users_df,adf)
+  }
+  return(all_users_df)
+}
+
