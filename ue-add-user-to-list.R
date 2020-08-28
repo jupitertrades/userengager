27799e3a4329a41002c612c097f4756145bc0205
simit patel <simitp@gmail.com>
2020-08-28 13:10:22 -0400

    up

--- ue-add-user-to-list.R
+++ ue-add-user-to-list.R
@@ -12,11 +12,11 @@
 
 ue_add_user_to_list <- function(ue_id,ue_key = Sys.getenv('ue_key'),user_id,list_id) {
   pb <- list(list = list_id)
-  post_response <- POST(glue("https://{ue_id}.user.com/api/public/users/:{user_id}/add_to_list/"),
+  post_response <- POST(glue("https://{ue_id}.user.com/api/public/users/{user_id}/add_to_list/"),
              add_headers(`Authorization` = glue('Token {ue_key}'),
                          `Content-Type` = 'application/json'),
              body=pb,encode = 'json')
-  
+
   return(post_response)
 }
 
--- ue-add-user.R
+++ ue-add-user.R
@@ -12,9 +12,10 @@
 
 ue_add_user <- function(ue_id,ue_key = Sys.getenv('ue_key'),email_address,f_name = NULL,l_name = NULL,c_id = NULL) {
   user_add_response <- POST(glue('https://{ue_id}.user.com/api/public/users/'),
-                             add_headers(`Authorization` = glue('Token {ue_key}')),
-                         body=list(email = email_address,first_name = f_name,last_name = l_name,
-                                   company_id = c_id),encode = 'json')
+                            add_headers(`Authorization` = glue('Token {ue_key}')),
+                            body=list(email = email_address,first_name = f_name,last_name = l_name,
+                                      company_id = c_id),encode = 'json') %>% content() %>% tibble(id = .$id,email = .$email) %>%
+    select(c(id,email)) %>% distinct()
   return(user_add_response)
 }
 
